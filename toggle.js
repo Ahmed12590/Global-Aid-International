356c744278d344cdc840f0b9d849218422349edf
Your Name <coderoofsitebuilders@gmail.com>
2024-11-22 21:51:19 +0500

    mobile menu

--- toggle.js
+++ toggle.js
@@ -2,7 +2,6 @@ const menuToggle = document.getElementById("menu-toggle");
 const mobileMenu = document.getElementById("mobile-menu");
 
 menuToggle.addEventListener("click", function () {
-  // Toggle the 'open' class for both menu and icon
   mobileMenu.classList.toggle("open");
   menuToggle.classList.toggle("open");
 });
