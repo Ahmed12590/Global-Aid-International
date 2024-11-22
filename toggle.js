7c2f55ee4f9c82376f80cec506b53c5be9690b55
Your Name <coderoofsitebuilders@gmail.com>
2024-11-22 23:02:53 +0500

    donate page

--- toggle.js
+++ toggle.js
@@ -2,6 +2,9 @@ const menuToggle = document.getElementById("menu-toggle");
 const mobileMenu = document.getElementById("mobile-menu");
 
 menuToggle.addEventListener("click", function () {
+  // Toggle the menu visibility
   mobileMenu.classList.toggle("open");
+
+  // Toggle the animation for the menu icon
   menuToggle.classList.toggle("open");
 });
