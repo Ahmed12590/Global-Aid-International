d3e15b807a390c71c55baa21b1c5c74e922b69ce
Your Name <coderoofsitebuilders@gmail.com>
2024-12-05 23:37:02 +0500

    faq page

--- toggle.js
+++ toggle.js
@@ -12,3 +12,17 @@ menuToggle.addEventListener("click", function () {
   menuToggle.classList.toggle("open");
   console.log("Menu icon class list:", menuToggle.classList); // Logs current classes on menu icon
 });
+
+
+
+
+
+// home page slider 
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function moveSlide(direction) {
+  currentSlide = (currentSlide + direction + slides.length) % slides.length;
+  document.querySelector('.slider').style.transform = `translateX(-${currentSlide * 100}%)`;
+}
